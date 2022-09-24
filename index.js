49ed7f58c4d1b4574a2779bf52cbea4dd8bb9f46
LAPTOP-KHOALE\Admin <leminkhoa@gmail.com>
2022-09-24 19:07:21 +0700

    add save function

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // camelCase
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 // intialize the count as 0
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let message = count + " - "
+    saveEl.textContent += message
+    countEl.textContent = 0
+    count = 0
 }
